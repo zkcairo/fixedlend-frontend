3db97a9061ef5b0659cc8c8845b421e4d9a6303c
zkcairo <167991601+zkcairo@users.noreply.github.com>
2025-07-20 12:20:26 +0200

    add tg link to the bottom

--- src/app/components/Bottom.tsx
+++ src/app/components/Bottom.tsx
@@ -3,13 +3,19 @@
 const Bottom = () => {
 
   return (
-    <div className="mb-16 text-center">
-    <button><a href="https://discord.gg/mN8gZUgFS7" target="_blank">Discord</a></button>
+  <>
+  <div className="text-center">
+    
     <button><a href="https://twitter.com/FixedLend" target="_blank">Twitter</a></button>
     <button><a href="https://docs.FixedLend.com">Docs</a></button>
     <button><a href="https://github.com/zkcairo" target="_blank">Github</a></button>
     <button><a href="/points">Points program</a></button>
   </div>
+  <div className="mb-16 text-center">
+    <button><a href="https://discord.gg/mN8gZUgFS7" target="_blank">Discord</a></button>
+    <button><a href="https://t.me/+x1dXX6x0A3MzNGU0" target="_blank">Telegram</a></button>
+  </div>
+  </>
   );
 };
 
