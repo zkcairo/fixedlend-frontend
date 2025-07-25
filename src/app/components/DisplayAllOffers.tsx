0f7c43d2de3ce3a34ac6dd4433c1f4272f8a09a4
zkcairo <167991601+zkcairo@users.noreply.github.com>
2025-07-25 13:52:55 +0200

    fix dec in loans

--- src/app/components/DisplayAllOffers.tsx
+++ src/app/components/DisplayAllOffers.tsx
@@ -29,8 +29,8 @@ const AllOffers = ({ offers, loading, type, me, labelButton, action, category }:
   const renderOffer = (offer: any, index: number) => (
     <div key={index} className="flex flex-col gap-y-1 p-4 border border-green-500/30">
       <h3 className="text-xl font-bold tracking-wider">Offer #{offer.id.toString()}</h3>
-      <p>Available: {formatCurrency(Number(offer.amount_available))} {categoryName(category)}</p>
-      {me && <p>Total Size: {formatCurrency(Number(offer.total_amount))} {categoryName(category)}</p>}
+      <p>Available: {formatCurrency(Number(offer.amount_available), 10**7)} {categoryName(category)}</p>
+      {me && <p>Total Size: {formatCurrency(Number(offer.total_amount), 10**7)} {categoryName(category)}</p>}
       <p>Yield: {formatYield(Number(offer.price.rate)) + (me ? 0 : type === "lend" ? 1 : -1)}% APR</p>
       <p>Duration: {formatTime(Number(offer.price.minimal_duration) / 3600)} - {formatTime(Number(offer.price.maximal_duration) / 3600)}</p>
       {type === "borrow" && <p>Collateral: <DisplayCollateral offer={offer} me={me} /></p>}
@@ -53,11 +53,11 @@ const AllOffers = ({ offers, loading, type, me, labelButton, action, category }:
       return (
           <div key={index} className="flex flex-col gap-y-1 p-4 border border-green-500/30">
               <h3 className="text-xl font-bold tracking-wider">Loan #{loan.id.toString()} (you {loanType})</h3>
-              <p>Amount: {formatCurrency(Number(loan.amount))} {categoryName(category)}</p>
+              <p>Amount: {formatCurrency(Number(loan.amount), 10**7)} {categoryName(category)}</p>
               <p>APR: {formatYield(rate)}%</p>
               <p>Repay After: {formatDate(loan.date_taken + loan.minimal_duration)}</p>
               <p>Repay Before: {formatDate(loan.date_taken + loan.maximal_duration)}</p>
-              <p>Current Value: {formatCurrency(interest)} {categoryName(category)}</p>
+              <p>Current Value: {formatCurrency(interest, 10**7)} {categoryName(category)}</p>
               {!isBorrow && <p>Interest Earned: {formatCurrency(interest - Number(loan.amount), 10**18)} {categoryName(category)}</p>}
               <button
                   className="mt-2"
